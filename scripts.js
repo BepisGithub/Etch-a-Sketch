1beb8b791eb82ff5c49e30ec91d54177579c9f83
bepis <luqdek@gmail.com>
2020-12-27 19:53:58 +0000

    Added some temp styling to see the grid

--- scripts.js
+++ scripts.js
@@ -3,13 +3,21 @@ let gridArray = [];
 const totalGridNumber = 256;
 const gridContainer = document.createElement("div");
 gridContainer.id = "gridContainer";
+
+
+
+// gridContainer.style.width = "800px"; //50px * 16
+// gridContainer.style.height = "800px"; //50px * 16
+
+
+//end 
 body.appendChild(gridContainer);
 for(i=0;i<256;i++){
     gridArray[i] = document.createElement("div");
     gridContainer.appendChild(gridArray[i]);
-    console.log(i);
-    console.log(gridArray[i]);
+    //temp
+    gridArray[i].style.border = "solid 1px black";
+    // end temp
     gridArray[i].style.height = "50px";
     gridArray[i].style.width = "50px";
 }
-
