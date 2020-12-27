747bc94dda29fda15fcd116dbce4e7c26f7a1e68
bepis <luqdek@gmail.com>
2020-12-27 19:34:19 +0000

    Added a for loop that creates 256 divs

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const body = document.querySelector("body");
+let gridArray = [];
+const totalGridNumber = 256;
+for(i=0;i<256;i++){
+    gridArray[i] = document.createElement("div");
+    body.appendChild(gridArray[i]);
+    console.log(i);
+    console.log(gridArray[i]);
+    gridArray[i].style.height = "50px";
+    gridArray[i].style.width = "50px";
+}
+
