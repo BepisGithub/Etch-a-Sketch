da7f6e925a6b7f857f6100cc76a1acf9d0a88604
bepis <luqdek@gmail.com>
2020-12-27 19:38:08 +0000

    Added a grid container for the purposes of styling all the grids

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const body = document.querySelector("body");
 let gridArray = [];
 const totalGridNumber = 256;
+const gridContainer = document.createElement("div");
+gridContainer.id = "gridContainer";
+body.appendChild(gridContainer);
 for(i=0;i<256;i++){
     gridArray[i] = document.createElement("div");
-    body.appendChild(gridArray[i]);
+    gridContainer.appendChild(gridArray[i]);
     console.log(i);
     console.log(gridArray[i]);
     gridArray[i].style.height = "50px";
