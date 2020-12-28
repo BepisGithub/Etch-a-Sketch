7940b22d4e2ae2a88fce742c742db2575379b72c
bepis <luqdek@gmail.com>
2020-12-28 09:24:07 +0000

    Fixed the event listener, i needed to pass in e to the anon func and then use that to style

--- scripts.js
+++ scripts.js
@@ -20,8 +20,18 @@ for(i=0;i<256;i++){
     // end temp
     gridArray[i].style.height = "50px";
     gridArray[i].style.width = "50px";
-    gridArray[i].addEventListener("click", () => gridArray[i].); //new
-    //i think i need to create a spare var to do thje following:
-    // gridArray[i] = tempVar;
-    // tempVar.addEventListener("click, () => tempVar.addClass")
+    gridArray[i].addEventListener("mouseover", (e) =>  e.target.style.backgroundColor = "grey"); //new
+    // gridArray[i].addEventListener("click", (e) => e.target.style.backgroundColor = "pink"); //new
 }
+
+
+//// gridArray[1].addEventListener("click", gridArray[i].setAttribute("style", "background-color: red")
+
+
+//I FOUND THE ISSUE. THE EVENT LISTENER IS SET UP ALWAYS BUT IN THE FUNCTION THAT IT CALLS IT STILL USES GRIDARRAY[I] BUT THE I GETS DESTROYED AS SOON AS WE LEAVE THE FOR LOOP
+//THIS MEANS WE NEED TO FIND SOME WAY TO SET THE EVENT LISTENER TO call a function that adjusts BG colour without the use of I... lol
+
+//myb set the id of each div to something done
+
+//use the id
+//gridArray[i].setAttribute("id",`container${i}`);
